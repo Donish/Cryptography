d79cf1051dd34332c2d35fed7315d3bff3e0159c
Zhuraev Donish <zhuraev2003@bk.ru>
2024-03-07 01:04:58 +0300

    fix permutation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
+import org.example.IBitOperator;
 import org.example.impl.BitOperator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BitOperator bitOperator = new BitOperator();
+
+        byte[] source = new byte[3];
+        source[0] = 5;
+        source[1] = 3;
+        source[2] = 103;
+
+        int[] pBlock = {1, 5, 12, 8, 9, 21, 23, 5, 4, 9, 11, 0};
+
+        byte[] permuted = bitOperator.bitPermutation(source, pBlock,
+                IBitOperator.bitIndexing.junior_to_senior,
+                IBitOperator.firstBit.ZERO);
+
+        for (byte el : source) {
+            System.out.println(el);
+            System.out.println(Integer.toBinaryString(el));
+        }
+
+        for (byte el : permuted) {
+            System.out.println(el);
+            System.out.println(Integer.toBinaryString(el));
+        }
+
     }
 }
--- src/main/java/org/example/impl/BitOperator.java
+++ src/main/java/org/example/impl/BitOperator.java
@@ -6,7 +6,17 @@ public class BitOperator implements IBitOperator {
 
     @Override
     public byte[] bitPermutation(byte[] source, int[] pBlock, bitIndexing indexRule, firstBit startRule) {
-        int sourceSize = source.length;
+        if (source == null || pBlock == null || indexRule == null || startRule == null) {
+            throw new RuntimeException("null arg provided");
+        }
+        for (int el : pBlock) {
+            if ((el == 0 && startRule == firstBit.ONE) ||
+                    el < 0 ||
+                    (startRule == firstBit.ZERO && el >= source.length * 8) ||
+                    (startRule == firstBit.ONE && el > source.length * 8))
+                throw new RuntimeException("incorrect pBlock elements");
+        }
+
         int bitQuantity = pBlock.length;
         int byteSize = bitQuantity % 8 == 0 ? bitQuantity / 8 : bitQuantity / 8 + 1;
         int offset = 0;
@@ -15,16 +25,26 @@ public class BitOperator implements IBitOperator {
         }
 
         byte[] result = new byte[byteSize];
-        if (indexRule == bitIndexing.junior_to_senior)
-            junToSenPermutation(source, result, pBlock, offset);
-        else
-            senToJunPermutation(source, result, pBlock, offset);
+        if (indexRule == bitIndexing.junior_to_senior) junToSenPermutation(source, result, pBlock, offset);
+        else senToJunPermutation(source, result, pBlock, offset);
 
         return result;
     }
 
+    //    indexing from zero
     private void junToSenPermutation(byte[] source, byte[] result, int[] pBlock, int offset) {
+        byte temp;
+        for (int i = 0; i < pBlock.length; i++) {
+            int byteIdx = pBlock[i] / 8; //(pBlock[i] - 1) / 8;
+            byteIdx = source.length - byteIdx - 1;
+            temp = source[byteIdx];
+            int bitIdx = pBlock[i] % 8;
+            int bit = (temp >> bitIdx) & 1;
 
+            int resultByteIdx = (pBlock.length - (i + 1)) / 8;
+            result[resultByteIdx] <<= 1;
+            result[resultByteIdx] |= (byte) bit;
+        }
     }
 
     private void senToJunPermutation(byte[] source, byte[] result, int[] pBlock, int offset) {
