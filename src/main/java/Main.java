2ae7a7c761829309f70cd1ea218f4b052172d3d8
Zhuraev Donish <zhuraev2003@bk.ru>
2024-03-09 17:26:30 +0300

    permutation done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import org.example.IBitOperator;
 import org.example.impl.BitOperator;
 
 public class Main {
@@ -7,14 +6,16 @@ public class Main {
 
         byte[] source = new byte[3];
         source[0] = 5;
-        source[1] = 3;
+        source[1] = -3;
         source[2] = 103;
+        System.out.println(Integer.toBinaryString(-5));
+        System.out.println(Integer.toBinaryString(5));
 
-        int[] pBlock = {22, 23, 21, 20, 22, 23, 20, 1, 5, 12, 8, 9, 21, 23, 5, 5, 23, 21, 22};
+        int[] pBlock = {1, 5, 12, 8, 15, 21, 23, 5, 4, 9, 11, 0};
 
         byte[] permuted = bitOperator.bitPermutation(source, pBlock,
-                IBitOperator.bitIndexing.senior_to_junior,
-                IBitOperator.firstBit.ZERO);
+                BitOperator.bitIndexing.junior_to_senior,
+                BitOperator.firstBit.ZERO);
 
         for (byte el : permuted) {
             System.out.println(el);
--- src/main/java/org/example/impl/BitOperator.java
+++ src/main/java/org/example/impl/BitOperator.java
@@ -31,8 +31,7 @@ public class BitOperator implements IBitOperator {
     private void junToSenPermutation(byte[] source, byte[] result, int[] pBlock, int offset) {
         byte temp;
         for (int i = 0; i < pBlock.length; i++) {
-            int byteIdx = (pBlock[i] - offset) / 8;
-            byteIdx = source.length - byteIdx - 1;
+            int byteIdx = source.length - ((pBlock[i] - offset) / 8) - 1;
             temp = source[byteIdx];
             int bitIdx = (pBlock[i] - offset) % 8;
             int bit = (temp >> bitIdx) & 1;
