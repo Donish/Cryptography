da729108044cc2e4d2b5b38657c6f07748e20f2d
Zhuraev Donish <zhuraev2003@bk.ru>
2024-04-05 21:26:34 +0300

    OFB done

--- src/main/java/org/example/impl/cipher_mode_impl/OFBMode.java
+++ src/main/java/org/example/impl/cipher_mode_impl/OFBMode.java
@@ -2,18 +2,44 @@ package org.example.impl.cipher_mode_impl;
 
 import org.example.interfaces.IAlgorithm;
 import org.example.interfaces.ICipherMode;
+import org.example.utils.BitUtils;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class OFBMode implements ICipherMode {
 
     @Override
     public byte[] encryptWithMode(byte[] text, byte[] IV, List<String> parameters, IAlgorithm algorithm, int blockSize) {
-        return new byte[0];
+        byte[] result = new byte[text.length];
+        byte[] prevBlock = IV;
+        int blocksCount = text.length / blockSize;
+
+        for (int i = 0; i < blocksCount; i++) {
+            int idx = i * blockSize;
+            byte[] block = Arrays.copyOfRange(text, idx, idx + blockSize);
+            prevBlock = algorithm.encryptBlock(prevBlock);
+            byte[] encryptedBlock = BitUtils.xorArrays(block, prevBlock);
+            System.arraycopy(encryptedBlock, 0, result, idx, encryptedBlock.length);
+        }
+
+        return result;
     }
 
     @Override
     public byte[] decryptWithMode(byte[] cipheredText, byte[] IV, List<String> parameters, IAlgorithm algorithm, int blockSize) {
-        return new byte[0];
+        byte[] result = new byte[cipheredText.length];
+        byte[] prevBlock = IV;
+        int blocksCount = cipheredText.length / blockSize;
+
+        for (int i = 0; i < blocksCount; i++) {
+            int idx = i * blockSize;
+            byte[] block = Arrays.copyOfRange(cipheredText, idx, idx + blockSize);
+            prevBlock = algorithm.encryptBlock(prevBlock);
+            byte[] decryptedBlock = BitUtils.xorArrays(block, prevBlock);
+            System.arraycopy(decryptedBlock, 0, result, idx, decryptedBlock.length);
+        }
+
+        return result;
     }
 }
